37d05e744664fc6b7b4958ef6b9a5f68c98f15d6
obodrov <bodrovphone@gmail.com>
2017-05-24 23:54:46 +0200

    '0.0.1'

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-
+import {  ProgressBar } from 'react-bootstrap';
 //Custom components
 import Question from './components/question';
 
 class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+        questionNumber: 0,
+        responses: []
+      };
+    }
   render() {
     return (
-      <section id="questions" class="question-box">
-        <div class="block-fill-height">
-          <div class="block block-all block-xs-middle">
-            <div class="container">
+      <section id="questions" className="question-box">
+        <div className="container-fluid custom-header">
+          <div className="custom-progress-wrapper">
+            <ProgressBar now={1} />
+          </div>  
+        </div>
+        <div className="block-fill-height">
+          <div className="block block-all block-xs-middle">
+            <div className="container">
               <Question />
             </div>
           </div>
--- src/components/question.js
+++ src/components/question.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
-
+import { Button } from 'react-bootstrap';
 export default class Question extends Component {
   render() {
     return (
-      <h1>blabla</h1>
+        <div className="custom-question-centered">
+          <h1>Let's get started</h1>
+          <p>We just to grab some information of your company in order to understand your needs well, and provide you with the best solutions for you.</p>
+          <Button onClick={function(){console.log('haha')}} bsStyle="primary">NEXT</Button>
+        </div>
     );
   }
 }
